8036dafa8a47cc9ca3c028d374fe02f6ace2f984
danilaBel <danilabelozarovich@gmail.com>
2020-07-21 13:32:36 +0300

    start commit

---
+++ EnternetShop/EnternetShop.Application/Interfaces/IEnternetShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EnternetShop.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace EnternetShop.Application.Interfaces
+{
+    public interface IEnternetShopDbContext
+    {
+        DbSet<Category> Categories { get; set; }
+        DbSet<Filter> Filters { get; set; }
+        DbSet<Product> Products { get; set; }
+
+        public void SaveChangesAsync(CancellationToken cn);
+    }
+}
